1ea3a9e60753da1736a212f31c38225e936845de
Thomas Lapierre <thomaslapierre@orange.fr>
2019-04-14 16:03:04 +0200

    ajout de case colorée ainsi que de ces méthodes

---
+++ src/mesClasses/CaseColoree.java
@@ -0,0 +1,15 @@
+package mesClasses;
+
+public class CaseColoree extends Case {
+
+	private Couleur couleur;
+	
+	public CaseColoree(Couleur couleur) {
+		this.couleur=couleur;
+	}
+	
+	public Couleur getCouleur() {
+		return couleur;
+	}
+
+}
