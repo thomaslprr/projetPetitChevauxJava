46e086f835a89477bcefea578e5e99c3cccbc004
Thomas Lapierre <thomaslapierre@orange.fr>
2019-04-14 15:46:52 +0200

    ajout des méthodes dans la classe "Case"

--- src/mesClasses/Case.java
+++ src/mesClasses/Case.java
@@ -1,9 +1,28 @@
 package mesClasses;
 
+import java.util.ArrayList;
+
 public class Case {
 
 	public Case() {
 		
 	}
+	
+	public ArrayList<Pion> getChevaux(){
+		
+		return null;
+	}
+	
+	public void ajouteCheval(Pion pion) {
+		
+	}
 
+	public boolean peutPasser(Pion pion) {
+		
+	}
+	
+	public boolean peutSArreter(Pion pion) {
+		
+	}
+	
 }
