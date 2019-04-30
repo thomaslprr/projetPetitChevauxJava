74118f1c9449aa881799b7fc64ac49f826fb2a95
Thomas Lapierre <thomaslapierre@orange.fr>
2019-04-30 10:18:13 +0200

    création + ajout des méthodes de la classe partie

---
+++ src/mesClasses/Partie.java
@@ -0,0 +1,53 @@
+package mesClasses;
+
+import java.util.ArrayList;
+
+public class Partie {
+	private int de;
+	public Partie() {
+		
+	}
+	
+	public void initialiserJoueurs(int numJoueur) {
+		
+	}
+	
+	public void initialiserPlateau() {
+		
+	}
+	
+	public int lanceDe() {
+		int a = 0;
+		return a;
+	}
+	
+	public void jouerUnTour() {
+		
+	}
+	
+	public boolean estPartieTermine() {
+		return false;
+		
+	}
+	
+	public Joueur getJoueurCourrant() {
+		
+		return Joueur;
+	}
+	
+	public void setJoueurCourrant(Joueur j) {
+		
+	}
+	
+	public Plateau getPlateau() {
+		return Plateau;
+	}
+	
+	public ArrayList<Joueur> getJoueur(){
+		return null;
+	}
+	
+	public void mangerLesPions(Case c) {
+		
+	}
+}
